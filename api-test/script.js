20f3e591ecad6b9863be10c562dd51afe9ce4bde
Michael Douglas Alves Machado <michaeldouglasalvesmachado@gmail.com>
2026-01-19 00:02:44 -0300

    Fiz uma tela de loading
    É domingo e diminui um pouco meus estudos hoje, amanhã volta tudo.

--- api-test/script.js
+++ api-test/script.js
@@ -1,93 +1,93 @@
 
 const mainSection = document.querySelector('#mainSection')
+const header = document.querySelector('header')
+const loading = document.querySelector('#iLoading')
 
-window.addEventListener('DOMContentLoaded', buscarPost = () => {
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
-    .then((response) => response.json())
-    .then((posts) => {
-        posts.sort((a, b) => b - b.id - a.id).map((item) => {
-            // div .post
-            const createPost = document.createElement('div')
-            createPost.classList.add('post')
-            mainSection.appendChild(createPost)
+window.addEventListener('DOMContentLoaded', buscarPost = async () => {
+    const responseUser = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const users = await responseUser.json() 
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const posts = await response.json() 
+    await posts.sort((a, b) => b.id - a.id).map((item) => {
+        // div .post
+        const createPost = document.createElement('div')
+        createPost.classList.add('post')
+        mainSection.appendChild(createPost)
 
-            // title do post
-            const createTitle = document.createElement('h3')
-            createTitle.innerText = item.title
-            createPost.appendChild(createTitle)
+        // title do post
+        const createTitle = document.createElement('h3')
+        createTitle.innerText = item.title
+        createPost.appendChild(createTitle)
 
-            // text do post
-            const createTextPost = document.createElement('p')
-            createTextPost.innerText = item.body
-            createPost.appendChild(createTextPost)
+        // text do post
+        const createTextPost = document.createElement('p')
+        createTextPost.innerText = item.body
+        createPost.appendChild(createTextPost)
 
-            // container do comentar
-            const createContainerButtonComentar = document.createElement('div')
-            createContainerButtonComentar.classList.add('reactButtons')
-            createPost.appendChild(createContainerButtonComentar)
+        // container do comentar
+        const createContainerButtonComentar = document.createElement('div')
+        createContainerButtonComentar.classList.add('reactButtons')
+        createPost.appendChild(createContainerButtonComentar)
 
-            // button de comentar
-            const createButtonComentar = document.createElement('a')
-            createButtonComentar.innerText =   'Comentar'
-            createButtonComentar.href = ''
-            createContainerButtonComentar.appendChild(createButtonComentar)
-            const abrirComentar = (event) => {
-                event.preventDefault(); 
+        // button de comentar
+        const createButtonComentar = document.createElement('a')
+        createButtonComentar.innerText =   'Comentar'
+        createButtonComentar.href = ''
+        createContainerButtonComentar.appendChild(createButtonComentar)
+        const abrirComentar = async (event) => {
+            event.preventDefault(); 
 
-                // Container dos comentarios
-                const createContainerComentarios = document.createElement('div')
-                createContainerComentarios.classList.add('divComentario')
-                createPost.appendChild(createContainerComentarios)
-
-                // add comentario
-                fetch(`https://jsonplaceholder.typicode.com/comments?postId=${item.id}`)
-                .then((response) => response.json())
-                .then((coment) => {
-                    coment.forEach((comentItem) => {
-                        const newComent = `<div class="comentario"><h4 class="nomeComentario">${comentItem.name}</h4><p class="comantarioPost"> ${comentItem.body}</p></div>`
-                        createContainerComentarios.insertAdjacentHTML('beforeend', newComent)
-                    })
+            // Container dos comentarios
+            const createContainerComentarios = document.createElement('div')
+            createContainerComentarios.classList.add('divComentario')
+            createPost.appendChild(createContainerComentarios)
+            const responsePost = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${item.id}`)
+            const coment = await responsePost.json()
+                coment.forEach((comentItem) => {
+                    const newComent = `<div class="comentario"><h4 class="nomeComentario">${comentItem.name}</h4><p class="comantarioPost"> ${comentItem.body}</p></div>`
+                    createContainerComentarios.insertAdjacentHTML('beforeend', newComent)
                 })                  
 
 
-                // Container dos comentarios
-                const createContainerComentar = document.createElement('div')
-                createContainerComentar.classList.add('comentar')
-                createPost.appendChild(createContainerComentar)
+            // Container dos comentarios
+            const createContainerComentar = document.createElement('div')
+            createContainerComentar.classList.add('comentar')
+            createPost.appendChild(createContainerComentar)
 
-                // Input Nome dos comentarios
-                const createInputComentarNome = document.createElement('input')
-                createInputComentarNome.name = 'nome'
-                createInputComentarNome.id = 'iNome'
-                createInputComentarNome.type = 'text'
-                createInputComentarNome.placeholder = 'Para comentar abaixo, digite um nome...'
-                createContainerComentar.appendChild(createInputComentarNome)
+            // Input Nome dos comentarios
+            const createInputComentarNome = document.createElement('input')
+            createInputComentarNome.name = 'nome'
+            createInputComentarNome.id = 'iNome'
+            createInputComentarNome.type = 'text'
+            createInputComentarNome.placeholder = 'Para comentar abaixo, digite um nome...'
+            createContainerComentar.appendChild(createInputComentarNome)
 
-                // Input Text post dos comentarios
-                const createInputComentarPost = document.createElement('input')
-                createInputComentarPost.name = 'post'
-                createInputComentarPost.id = 'iPost'
-                createInputComentarPost.type = 'text'
-                createInputComentarPost.placeholder = 'Comente com o nome acima...'
-                createContainerComentar.appendChild(createInputComentarPost)
+            // Input Text post dos comentarios
+            const createInputComentarPost = document.createElement('input')
+            createInputComentarPost.name = 'post'
+            createInputComentarPost.id = 'iPost'
+            createInputComentarPost.type = 'text'
+            createInputComentarPost.placeholder = 'Comente com o nome acima...'
+            createContainerComentar.appendChild(createInputComentarPost)
 
-                // Input Button enviar dos comentarios
-                const createInputComentarButton = document.createElement('input')
-                createInputComentarButton.type = 'button'
-                createInputComentarButton.value = 'Enviar'
-                createContainerComentar.appendChild(createInputComentarButton)
-                const apagarComentar = (event) => {
-                    event.preventDefault(); 
-                    createContainerComentarios.remove()
-                    createContainerComentar.remove()
-                    createButtonComentar.addEventListener('click', abrirComentar)
-                    createButtonComentar.removeEventListener('click', apagarComentar)
-                }   
-                createButtonComentar.removeEventListener('click', abrirComentar)
-                createButtonComentar.addEventListener('click', apagarComentar)
+            // Input Button enviar dos comentarios
+            const createInputComentarButton = document.createElement('input')
+            createInputComentarButton.type = 'button'
+            createInputComentarButton.value = 'Enviar'
+            createContainerComentar.appendChild(createInputComentarButton)
+            const apagarComentar = (event) => {
+                event.preventDefault(); 
+                createContainerComentarios.remove()
+                createContainerComentar.remove()
+                createButtonComentar.addEventListener('click', abrirComentar)
+                createButtonComentar.removeEventListener('click', apagarComentar)
+            }   
+            createButtonComentar.removeEventListener('click', abrirComentar)
+            createButtonComentar.addEventListener('click', apagarComentar)
 
-            }
-            createButtonComentar.addEventListener('click', abrirComentar)
-        })
+        }
+        createButtonComentar.addEventListener('click', abrirComentar)
     })
+    header.style.display = 'block'
+    loading.remove()
 })
